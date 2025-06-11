6f4957383fd74e602e839f275476d3d7bed8bbe0
hendrism <hendrism@gmail.com>
2025-06-11 10:58:59 -0400

    fixed database file name

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "sqlite:///" + os.path.join(BASE_DIR, "student_database.db"),
+        "sqlite:///" + os.path.join(BASE_DIR, "database.db"),
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
